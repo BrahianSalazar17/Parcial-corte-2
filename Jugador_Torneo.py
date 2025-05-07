acffeafa2dca251e6b7fe0b1bee124f7b2a43d36
BrahianSalazar17 <brahian-132@hotmail.com>
2025-05-07 18:21:13 -0500

    Add files via upload

---
+++ Jugador_Torneo.py
@@ -0,0 +1,24 @@
+class Jugador:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+class Torneo:
+    def __init__(self, ganador):
+        self.ganador = ganador
+
+class Encuentro:
+    def __init__(self, torneo, limite):
+        self.torneo = torneo
+        self.limite = limite
+
+# Ejemplo de uso
+jugador1 = Jugador("Carlos")
+torneo1 = Torneo(jugador1)
+encuentro1 = Encuentro(torneo1, 10)
+
+print("Ganador del torneo:", encuentro1.torneo.ganador.nombre)
+print("Límite del encuentro:", encuentro1.limite)
+
+
+
+
