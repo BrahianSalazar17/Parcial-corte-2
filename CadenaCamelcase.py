2241363045275f68d839ee967fd8a0b92c20f81b
BrahianSalazar17 <brahian-132@hotmail.com>
2025-05-07 18:08:30 -0500

    Add files via upload

---
+++ CadenaCamelcase.py
@@ -0,0 +1,29 @@
+def dividir_camel_case(cadena):
+    palabras = []
+    palabra_actual = ""
+
+    for letra in cadena:
+        if letra.isupper() and palabra_actual:
+            palabras.append(palabra_actual)
+            palabra_actual = letra
+        else:
+            palabra_actual += letra
+
+    if palabra_actual:
+        palabras.append(palabra_actual)
+
+    return palabras
+
+# Solicitar la cadena al usuario
+cadena = input("Ingrese una cadena en CamelCase: ")
+
+# Obtener palabras y mostrarlas
+palabras = dividir_camel_case(cadena)
+print(f"Número de palabras: {len(palabras)}")
+print("Palabras encontradas:")
+for palabra in palabras:
+    print(palabra)
+
+
+
+
